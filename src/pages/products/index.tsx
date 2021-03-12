0ab3e7a5fc176c68e11f07e6c248f80c2c587150
James <jhyu0210@gmail.com>
2021-03-12 09:45:15 +0900

    mock data for products

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,3 +1,4 @@
+import React, { Fragment, FunctionComponent } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
@@ -15,180 +16,82 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 library.add(faGithub, faTwitter, faPinterest, faYoutube, faMedium, faLinkedin);
 // import "../../styles/Home.module.css";
-
-import React, { Fragment, FunctionComponent } from "react";
+import data from "../../data";
 
 interface ProductsProps {}
 
 const Products: React.FunctionComponent<ProductsProps> = () => {
   let isGood = true;
+  console.log(data.products);
   return (
     <Fragment>
       <Head>
-        <title>reddit: front page of the internet</title>
+        <title>쇼핑몰</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* <pre>{JSON.stringify(data.products, null, 2)}</pre> */}
       {/* <div className="container flex pt-4">
         <div className="w-full px-4 md:w-160 md:p-0"> */}
       <ul className="flex flex-wrap p-4">
-        <li className="mb-3 mr-3">
-          <div className="relative rounded-sm shadow-lg w-60 h-80">
-            <img src="images/1_마레통3중.gif" alt="마레통3중" />
-            <div className="absolute top-0 left-0 m-2">
-              <span
-                className="p-1 mr-1 text-xs text-white rounded bg-purple-850"
-                style={{ fontSize: "10px" }}
-              >
-                특가
-              </span>
-              <span className="py-1 pl-2 text-sm font-bold rounded-full">
-                35,900원~
-              </span>
-            </div>
-            <Link href="/">
-              <div className="mt-2 text-sm font-bold text-center text-gray-700 transition duration-500 ease-in-out cursor-pointer hover:text-green-100">
-                마레 통3중 냄비
+        {data.products.map((product) => (
+          <li className="mb-3 mr-3" key={product._id}>
+            <h3>{product.category}</h3>
+            <div className="relative rounded-sm shadow-lg w-60 h-80">
+              <img src={product.image} alt="마레통3중" />
+              <div className="absolute top-0 left-0 m-2">
+                <span
+                  className="p-1 mr-1 text-xs text-white rounded bg-purple-850"
+                  style={{ fontSize: "10px" }}
+                >
+                  특가
+                </span>
+                <span className="py-1 pl-2 text-sm font-bold rounded-full">
+                  {product.price}원
+                </span>
               </div>
-            </Link>
+              <Link href="/">
+                <div className="mt-2 text-sm font-bold text-center text-gray-700 transition duration-500 ease-in-out cursor-pointer hover:text-green-100">
+                  {product.name}
+                </div>
+              </Link>
 
-            <h6
-              className="mt-2 text-xs tracking-tighter text-center text-gray-700"
-              style={{ fontSize: "10px" }}
-            >
-              레트로 감성의 감각적인 주물냄비 [인덕션 O] -유리뚜껑
-            </h6>
-
-            <div>
-              <FontAwesomeIcon
-                icon={isGood ? faStar : farStar}
-                className="h-4 text-green-100"
-              />
-              <FontAwesomeIcon icon={farStar} className="h-3 text-yellow-500" />
-              <FontAwesomeIcon icon={faStar} className="h-3 text-yellow-500" />
-              <FontAwesomeIcon icon={faStar} className="h-3 text-yellow-500" />
-              <FontAwesomeIcon icon={faStar} className="h-3 text-yellow-500" />
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="h-3 text-yellow-500"
-              />
-            </div>
-          </div>
-        </li>
-        <li className="mr-3">
-          <div className="relative rounded-sm shadow-lg w-60 h-80">
-            <img src="images/1_마레통3중.gif" alt="마레통3중" />
-            <div className="absolute top-0 left-0 m-2">
-              <span
-                className="p-1 mr-1 text-xs text-white rounded bg-purple-850"
+              <h6
+                className="mt-2 text-xs tracking-tighter text-center text-gray-700"
                 style={{ fontSize: "10px" }}
               >
-                특가
-              </span>
-              <span className="py-1 pl-2 text-sm font-bold rounded-full">
-                35,900원~
-              </span>
-            </div>
-            <Link href="/">
-              <h2 className="mt-2 text-sm font-bold text-center text-gray-700 cursor-pointer hover:text-green-100">
-                마레 통3중 냄비
-              </h2>
-            </Link>
+                {product.description}
+              </h6>
 
-            <h6
-              className="mt-2 text-xs tracking-tighter text-center text-gray-700"
-              style={{ fontSize: "10px" }}
-            >
-              레트로 감성의 감각적인 주물냄비 [인덕션 O] -유리뚜껑
-            </h6>
-          </div>
-        </li>
-        <li className="mr-3">
-          <div className="relative rounded-sm shadow-lg w-60 h-80">
-            <img src="images/1_마레통3중.gif" alt="마레통3중" />
-            <div className="absolute top-0 left-0 m-2">
-              <span
-                className="p-1 mr-1 text-xs text-white rounded bg-purple-850"
-                style={{ fontSize: "10px" }}
-              >
-                특가
-              </span>
-              <span className="py-1 pl-2 text-sm font-bold rounded-full">
-                35,900원~
-              </span>
+              <div>
+                <FontAwesomeIcon
+                  icon={isGood ? faStar : farStar}
+                  className="h-4 text-green-100"
+                />
+                <FontAwesomeIcon
+                  icon={farStar}
+                  className="h-3 text-yellow-500"
+                />
+                <FontAwesomeIcon
+                  icon={faStar}
+                  className="h-3 text-yellow-500"
+                />
+                <FontAwesomeIcon
+                  icon={faStar}
+                  className="h-3 text-yellow-500"
+                />
+                <FontAwesomeIcon
+                  icon={faStar}
+                  className="h-3 text-yellow-500"
+                />
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="h-3 text-yellow-500"
+                />
+              </div>
             </div>
-            <Link href="/">
-              <h2 className="mt-2 text-sm font-bold text-center text-gray-700 cursor-pointer hover:text-green-100">
-                마레 통3중 냄비
-              </h2>
-            </Link>
-
-            <h6
-              className="mt-2 text-xs tracking-tighter text-center text-gray-700"
-              style={{ fontSize: "10px" }}
-            >
-              레트로 감성의 감각적인 주물냄비 [인덕션 O] -유리뚜껑
-            </h6>
-          </div>
-        </li>
-        <li className="mr-3">
-          <div className="relative rounded-sm shadow-lg w-60 h-80">
-            <img src="images/1_마레통3중.gif" alt="마레통3중" />
-            <div className="absolute top-0 left-0 m-2">
-              <span
-                className="p-1 mr-1 text-xs text-white rounded bg-purple-850"
-                style={{ fontSize: "10px" }}
-              >
-                특가
-              </span>
-              <span className="py-1 pl-2 text-sm font-bold rounded-full">
-                35,900원~
-              </span>
-            </div>
-            <Link href="/">
-              <h2 className="mt-2 text-sm font-bold text-center text-gray-700 cursor-pointer hover:text-green-100">
-                마레 통3중 냄비
-              </h2>
-            </Link>
-
-            <h6
-              className="mt-2 text-xs tracking-tighter text-center text-gray-700"
-              style={{ fontSize: "10px" }}
-            >
-              레트로 감성의 감각적인 주물냄비 [인덕션 O] -유리뚜껑
-            </h6>
-          </div>
-        </li>
-        <li className="mr-3">
-          <div className="relative rounded-sm shadow-lg w-60 h-80">
-            <img src="images/1_마레통3중.gif" alt="마레통3중" />
-            <div className="absolute top-0 left-0 m-2">
-              <span
-                className="p-1 mr-1 text-xs text-white rounded bg-purple-850"
-                style={{ fontSize: "10px" }}
-              >
-                특가
-              </span>
-              <span className="py-1 pl-2 text-sm font-bold rounded-full">
-                35,900원~
-              </span>
-            </div>
-            <Link href="/">
-              <h2 className="mt-2 text-sm font-bold text-center text-gray-700 cursor-pointer hover:text-green-100">
-                마레 통3중 냄비
-              </h2>
-            </Link>
-
-            <h6
-              className="mt-2 text-xs tracking-tighter text-center text-gray-700"
-              style={{ fontSize: "10px" }}
-            >
-              레트로 감성의 감각적인 주물냄비 [인덕션 O] -유리뚜껑
-            </h6>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
-      {/* </div>
-      </div> */}
     </Fragment>
   );
 };
